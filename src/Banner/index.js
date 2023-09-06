b0d9a15277af89a3aad78297a94018ad0fba8e1d
JM-arlopes <jmiguelarlopes@gmail.com>
2023-09-05 23:58:44 -0300

    continuação

--- src/Banner/index.js
+++ src/Banner/index.js
@@ -1,18 +1,27 @@
 import React from "react";
-import { Image, StyleSheet, Dimensions } from "react-native"
+import { View, Image, StyleSheet, Dimensions, Text } from "react-native"
 import banner from '../../assets/img/banner.png'
 
 const width = Dimensions.get('screen').width
 
 const Banner = () => {
-    return <Image source={banner} style={styless.topo} />
+    return <>
+        <Image source={banner} style={styles.topo} />
+        <Text style={styles.titulo}>Esse é o nosso titulo</Text>
+    </>
 }
 
 
-const styless = StyleSheet.create({
+const styles = StyleSheet.create({
     topo: {
         maxWidth: "100%",
         height: width
+    },
+
+    titulo: {
+        position: "absolute",
+        top: 0,
+        color: "white",
     }
 })
 
