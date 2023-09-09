c75f92e02cebba4080f6c4ab338c44d3dc339acd
JM-arlopes <jmiguelarlopes@gmail.com>
2023-09-08 23:59:14 -0300

    inserindo novos faotres ao app

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Banner from './src/Banner'
+import Venda from './src/Venda';
 export default function App() {
   return (
     <View>
       {/* <StatusBar style="auto" /> */}
-      <Text>Me acharam </Text>
+      <Text>Me acharam</Text>
       <Banner></Banner>
-
+      <Venda></Venda>
     </View>
   );
 }
